651375df8ddb01626441de9c062fcc9c3579d393
Sören Tempel <soeren+git@soeren-tempel.net>
2022-08-07 14:43:00 +0200

    Main: Start working on actual ELF loading

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@
 module Main where
 
 import Utils
+import Memory
 import Decoder
 import Control.Monad.Catch
 import Data.Bits
@@ -19,6 +20,14 @@ import Data.Singletons.Sigma
 import qualified Data.ByteString.Lazy as BSL
 import System.FilePath
 import System.Environment
+
+-- Address at which the memory is supposed to be mapped.
+memoryStart :: Address
+memoryStart = 0x10000
+
+-- Translate an ELF physical address to a memory address.
+toMemAddr :: Word32 -> Address
+toMemAddr = (-) 0x10000
 
 readElf :: FilePath -> IO (Elf)
 readElf path = do
@@ -38,6 +47,22 @@ loadableSegments elfs = pure $ filter f elfs
     where
         f e@ElfSegment{..} = epType == PT_LOAD
         f _ = False
+
+-- Copy raw data from ELF to memory at the given absolute address.
+copyData :: [ElfXX a] -> Memory -> Address -> IO ()
+copyData [] _ _ = pure ()
+copyData ((d@ElfRawData{..}):xs) mem addr = do
+    storeByteString mem addr edData
+    copyData xs mem addr
+copyData f@(x:xs) mem addr = do
+    copyData xs mem addr
+
+-- Load all loadable segments of an ELF file into memory.
+loadElf :: Memory -> Elf -> IO ()
+loadElf mem (SELFCLASS32 :&: ElfList elfs) = do
+    loadable <- loadableSegments elfs
+    mapM (\ElfSegment{..} -> copyData epData mem $ toMemAddr epPhysAddr) loadable
+    pure ()
 
 textSection :: Elf -> IO (Maybe BSL.ByteString)
 textSection (SELFCLASS32 :&: ElfList elfs) = do
@@ -69,4 +94,5 @@ main = do
         then error "Accepting only a single file argument"
         else do
             elf <- readElf $ head args
+            --loadElf elf
             main' elf
