8524e8327bfbb580be5a4259e9efc76004f95386
Sören Tempel <soeren+git@soeren-tempel.net>
2022-08-13 08:05:27 +0200

    Regitster: add some doctests for RegIdx

--- app/Register.hs
+++ app/Register.hs
@@ -6,7 +6,15 @@ import Data.Ix
 import Data.Word
 import Data.Array.IO
 
--- Type to index the register file
+-- | Type to index for the register file.
+--
+-- >>> toEnum 12 :: RegIdx
+-- A2
+-- >>> toEnum 31 :: RegIdx
+-- T6
+-- >>> fromEnum (minBound :: RegIdx)
+-- 0
+--
 data RegIdx = Zero | RA | SP | GP | TP | T0 | T1 | T2 | FP
     | S1 | A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | S2 | S3
     | S4 | S5 | S6 | S7 | S8 | S9 | S10 | S11 | T3 | T4
