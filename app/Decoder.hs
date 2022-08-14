76011d68761435687664277bd390b16c0ce6b712
Sören Tempel <soeren+git@soeren-tempel.net>
2022-08-14 12:09:20 +0200

    Decoder: Add support for store word

--- app/Decoder.hs
+++ app/Decoder.hs
@@ -10,6 +10,7 @@ import Register
 -- Types used to represent immediates.
 type Iimm = Int32 -- XXX: Technically 12-bits
 type Uimm = Int32 -- XXX: Technically 20-bits
+type Simm = Iimm
 
 -- Type used to represent a decoded RISC-V instruction.
 data Instruction =
@@ -18,6 +19,7 @@ data Instruction =
     Andi  Iimm RegIdx RegIdx |
     Addi  Iimm RegIdx RegIdx |
     Lw    Iimm RegIdx RegIdx |
+    Sw    Simm RegIdx RegIdx |
     Lui   RegIdx Uimm |
     Auipc RegIdx Uimm |
     InvalidInstruction deriving (Show)
@@ -71,6 +73,10 @@ immI = fromIntegral . fromTwoscomp 12 . instrField 20 31
 immU :: Word32 -> Uimm
 immU = fromIntegral . fromTwoscomp 12 . instrField 12 31
 
+immS :: Word32 -> Simm
+immS i = fromTwoscomp 12 $ fromIntegral $
+    (instrField 25 31 i `shift` 5) .|.  (instrField 07 11 i)
+
 rs1 :: Word32 -> RegIdx
 rs1 = toEnum . fromIntegral . instrField 15 19
 
@@ -86,6 +92,7 @@ rd = toEnum . fromIntegral . instrField 7 11
 op_reg   = 0b0110011
 op_imm   = 0b0010011
 op_load  = 0b0000011
+op_store = 0b0100011
 op_lui   = 0b0110111
 op_auipc = 0b0010111
 
@@ -100,6 +107,9 @@ f3_and = 0b111
 -- Funct3 for load instructions.
 f3_loadw = 0b010
 
+-- Funct3 for store instructions.
+f3_storew = 0b010
+
 -- Type for an R-Type instruction (three register operands).
 type RTypeInstr = (RegIdx -> RegIdx -> RegIdx -> Instruction)
 
@@ -111,6 +121,12 @@ type ITypeInstr = (Iimm -> RegIdx -> RegIdx -> Instruction)
 
 invalid_itype :: ITypeInstr
 invalid_itype _ _ _ = InvalidInstruction
+
+-- Type for an S-Type instruction (two register, one S-Immediate).
+type STypeInstr = (Simm -> RegIdx -> RegIdx -> Instruction)
+
+invalid_stype :: STypeInstr
+invalid_stype _ _ _ = InvalidInstruction
 
 ------------------------------------------------------------------------
 
@@ -139,11 +155,19 @@ decode_load instr
     where
         f3 = funct3 instr
 
+-- Decode store instructions.
+decode_store :: Word32 -> STypeInstr
+decode_store instr
+    | f3 == f3_storew = Sw
+    where
+        f3 = funct3 instr
+
 decode' :: Word32 -> Word32 -> Instruction
 decode' instr opcode
     | opcode == op_reg   = decode_reg instr (rd instr) (rs1 instr) (rs2 instr)
     | opcode == op_imm   = decode_imm instr (immI instr) (rd instr) (rs1 instr)
     | opcode == op_load  = decode_load instr (immI instr) (rd instr) (rs1 instr)
+    | opcode == op_store = decode_store instr (immS instr) (rs1 instr) (rs2 instr)
     | opcode == op_lui   = Lui (rd instr) (immU instr)
     | opcode == op_auipc = Auipc (rd instr) (immU instr)
     | otherwise          = InvalidInstruction
@@ -160,6 +184,8 @@ decode' instr opcode
 -- Lw (-4) A0 A0
 -- >>> decode 0x00000297
 -- Auipc T0 0
+-- >>> decode 0x00102423
+-- Sw 8 Zero RA
 --
 decode :: Word32 -> Instruction
 decode instr = decode' instr $ opcode instr
