1077d4e98ff3343fab6fa4a3078f662a6e438015
TobiasB91 <tobbra91@gmail.com>
2022-10-13 15:01:04 +0200

    coproduct type and logging dsl

---
+++ src/Interpreter/Logging/InstructionFetch.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+module InstructionFetch where
+
+import Control.Monad.Free
+import Control.Monad.Free.TH
+import Types (Address)
+import Decoder (Instruction)
+import Numeric (showHex)
+
+data LogInstructionFetch r = 
+    LogInstructionFetch Address Instruction r deriving Functor 
+
+type LogInstructionFetchM = Free LogInstructionFetch
+
+makeFree ''LogInstructionFetch
+
+-- default log interpreter
+runIO :: Free LogInstructionFetch a -> IO a
+runIO = iterM run' where 
+    run' (LogInstructionFetch addr inst next) = do
+        putStrLn $ showHex addr $ ": " ++ show inst
+        next
