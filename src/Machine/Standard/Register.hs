061a48fcd0c3d3e8a14ec6e43b9f4c62fc9e6c38
Sören Tempel <soeren+git@soeren-tempel.net>
2022-11-04 03:31:57 +0100

    Register: Use boxed IOArray for register values
    IOArray has worse performance than IOUArray, however, IOUArray
    can also be used with certain types, i.e. in the current form
    the RegisterFile cannot be re-used with custom data types.
    
    See: https://wiki.haskell.org/Performance/Arrays

--- src/Machine/Standard/Register.hs
+++ src/Machine/Standard/Register.hs
@@ -7,21 +7,21 @@ import Data.Ix ( Ix )
 import Data.Word ( Word32 )
 import Data.Int ( Int32 )
 import Data.Array.IO
-    ( Ix, IOUArray, getElems, readArray, writeArray, MArray(newArray) )
+    ( Ix, IOArray, getElems, readArray, writeArray, MArray(newArray) )
 import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
 import Control.Monad (unless)
 
 -- Register file addressed by RegIdx containing Word32.
 data RegisterFile a = RegisterFile { pc   :: IORef Word32
-                                   , regs :: IOUArray RegIdx a
+                                   , regs :: IOArray RegIdx a
                                    }
 
 -- Create a new register file.
-mkRegFile :: MArray IOUArray a IO => a -> IO (RegisterFile a)
+mkRegFile :: a -> IO (RegisterFile a)
 mkRegFile defValue = RegisterFile <$> newIORef 0 <*> newArray (minBound, maxBound) defValue
 
 -- Dump all register values.
-dumpRegs :: (Integral a, MArray IOUArray a IO) => RegisterFile a -> IO String
+dumpRegs :: (Integral a) => RegisterFile a -> IO String
 dumpRegs = fmap (foldr (\(a, v) s -> show a ++ "\t= 0x" ++ showHex (fromIntegral v :: Word32) "\n" ++ s) ""
         . zip [(minBound :: RegIdx)..maxBound]) . getElems . regs
 
@@ -29,12 +29,12 @@ dumpRegs = fmap (foldr (\(a, v) s -> show a ++ "\t= 0x" ++ showHex (fromIntegral
 
 -- Read register value at given register index.
 -- For the zero register, value 0 is always returned.
-readRegister :: MArray IOUArray a IO => RegisterFile a -> RegIdx -> IO a
+readRegister :: RegisterFile a -> RegIdx -> IO a
 readRegister = readArray . regs
 
 -- Write register at given register index.
 -- Writes to the zero register are ignored.
-writeRegister :: MArray IOUArray a IO => RegisterFile a -> RegIdx -> a -> IO ()
+writeRegister :: RegisterFile a -> RegIdx -> a -> IO ()
 writeRegister RegisterFile{regs=r} idx val = unless (idx == Zero) $ writeArray r idx val
 
 -- Read program counter value.
