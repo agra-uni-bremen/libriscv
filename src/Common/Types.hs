7e864eefd9d5ad0bd294c25da3e8b208d5d15c2c
Sören Tempel <soeren+git@soeren-tempel.net>
2022-11-08 01:21:22 +0100

    Common: Derive Read type class for RegIdx type

--- src/Common/Types.hs
+++ src/Common/Types.hs
@@ -27,7 +27,7 @@ type Address = Word32
 data RegIdx = Zero | RA | SP | GP | TP | T0 | T1 | T2 | FP
     | S1 | A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | S2 | S3
     | S4 | S5 | S6 | S7 | S8 | S9 | S10 | S11 | T3 | T4
-    | T5 | T6 deriving (Ord, Eq, Ix, Bounded, Show, Enum)
+    | T5 | T6 deriving (Ord, Eq, Ix, Bounded, Show, Enum, Read)
 
 ------------------------------------------------------------------------
 
