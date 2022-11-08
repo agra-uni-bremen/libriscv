e968292cf542f9fd13be642721ed1f9bb351b184
Sören Tempel <soeren+git@soeren-tempel.net>
2022-11-08 22:56:16 +0100

    app: Move option parsing code to library

--- app/Main.hs
+++ app/Main.hs
@@ -1,65 +1,18 @@
 module Main where
 
-import Loader
-import Data.Word ( Word32 )
 import System.Environment ()
-import GHC.IO.StdHandles ( stdout )
 import Options.Applicative
-    ( (<**>),
-      argument,
-      auto,
-      fullDesc,
-      header,
-      help,
-      info,
-      long,
-      metavar,
-      option,
-      progDesc,
-      short,
-      str,
-      switch,
-      value,
-      execParser,
-      helper,
-      Parser )
 import Control.Monad (when)
 import Control.Monad.Freer
+
+import Loader
 import Spec.AST
-import Common.Types (Address)
+import CmdLine
 import Effects.Logging.InstructionFetch
 import Machine.Standard.Interpreter
 
-data CmdArgs = CmdArgs
-    { memAddr  :: Word32
-    , memStart :: Word32
-    , trace    :: Bool
-    , putRegs  :: Bool
-    , file     :: String }
-
-cmdArgs :: Parser CmdArgs
-cmdArgs = CmdArgs
-    <$> option auto
-        ( long "memory-start"
-       <> short 'm'
-       <> value 0x10000 )
-    <*> option auto
-        ( long "memory-size"
-       <> short 's'
-       <> value (1024 * 1024 * 1) -- 1 MB RAM
-       <> help "Size of the memory region" )
-    <*> switch
-        ( long "trace"
-       <> short 't'
-       <> help "Whether to trace all executed instructions" )
-    <*> switch
-        ( long "registers"
-       <> short 'r'
-       <> help "Whether to print all register values" )
-    <*> argument str (metavar "FILE")
-
-main' :: CmdArgs -> IO ()
-main' (CmdArgs memAddr memSize trace putReg fp) = do
+main' :: BasicArgs -> IO ()
+main' (BasicArgs memAddr memSize trace putReg fp) = do
     state <- mkArchState memAddr memSize
     entry <- loadExecutable fp state
 
@@ -79,7 +32,6 @@ main' (CmdArgs memAddr memSize trace putReg fp) = do
 main :: IO ()
 main = main' =<< execParser opts
     where
-        opts = info (cmdArgs <**> helper)
+        opts = info (basicArgs <**> helper)
             ( fullDesc
-           <> progDesc "Execute RV32I machine code"
-           <> header "riscv-tiny - a simulator for RV32I" )
+           <> progDesc "Concrete execution of RV32I machine code")
