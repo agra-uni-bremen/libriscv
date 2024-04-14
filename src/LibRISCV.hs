e14490fe29a493f0c3c7c6528f6d22e3cee8382e
Sören Tempel <tempel@uni-bremen.de>
2024-04-14 13:27:56 +0200

    Remove obsolete Conversion instance

--- src/LibRISCV.hs
+++ src/LibRISCV.hs
@@ -6,7 +6,6 @@ import Data.Word
 import Data.Ix
 import qualified Data.ByteString.Lazy as BSL
 import Data.BitVector
-import Conversion (Conversion (convert))
 
 type Signed32 = Int32
 type Unsigned32 = Word32
@@ -34,8 +33,3 @@ data RegIdx = Zero | RA | SP | GP | TP | T0 | T1 | T2 | FP
     | S1 | A0 | A1 | A2 | A3 | A4 | A5 | A6 | A7 | S2 | S3
     | S4 | S5 | S6 | S7 | S8 | S9 | S10 | S11 | T3 | T4
     | T5 | T6 deriving (Ord, Eq, Ix, Bounded, Show, Enum, Read)
-
-------------------------------------------------------------------------
-
-instance Conversion BV Word32 where  
-    convert = fromIntegral
